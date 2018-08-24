cff611241bd53f3465fdaf655740e982c2e1cc47
Phil Hawksworth <phil@netify.com>
2018-08-23 17:28:12 -0700

    submission events

---
+++ src/lambda/submission-created.js
@@ -0,0 +1,35 @@
+const request = require('request');
+var axios    = require('axios');
+
+// load environment variables
+require('dotenv').config()
+
+
+export function handler(event, context, callback) {
+
+  console.log('event.payload :', event.payload);
+
+  // we'll fetch hooks from the subscriptions form entries
+  var oauth_token = process.env.NETLIFY_TOKEN;
+  var formID = "5b7f4899daeb0a4d15ee3998";
+  var url = `https://api.netlify.com/api/v1/forms/${formID}/submissions/?access_token=${oauth_token}`;
+
+  axios.get(url)
+    .then(function (response) {
+        var body = response;
+        console.log('response :', response);
+        // hooks.forEach(url => {
+        //   request.post(url);
+        //   console.log('Notifying :', url);
+        // });
+        // console.log(`Deploy hooks for ${hooks.length} subscribers were called`);
+        return;
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+};
+
+
